7072f3b44530297aef770a3c9cc051af2cd0b191
Student <omarham206@hotmail.com>
2023-10-06 10:12:41 +0300

    Initial commit

---
+++ app/src/main/java/com/example/addrestactivity/User.java
@@ -0,0 +1,42 @@
+package com.example.addrestactivity;
+
+public class User {
+    String firstName, lastName, phone, bio;
+
+    public User() {
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+
+
+}
